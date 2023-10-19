2ed28bf77741cb3b0729f655c283de84f4de5255
Unbounder1 <ryadon12@gmail.com>
2023-10-18 23:41:29 -0400

    added text

--- src/components/hover_animation.jsx
+++ src/components/hover_animation.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 
-function  HoverComponents(){
-    const [hovered, setHovered] = React.useState(false);
+function HoverComponents(props) {
+    const [hovered, setHovered] = useState(false);
 
     const handleMouseEnter = () => {
         setHovered(true);
-    }
+    };
+
     const handleMouseLeave = () => {
         setHovered(false);
-    }
+    };
 
     return (
         <div
-        className={hovered ? 'hovered-bg': ''}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        ></div>
+            className={hovered ? 'hovered-bg' : ''}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            style={{ fontSize: '10vw' }}
+        >
+            {props.message}
+        </div>
     );
 }
 
