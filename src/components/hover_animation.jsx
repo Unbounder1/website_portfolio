e211fd040380daa18f9fe66a5f69aad7d1d86ca7
Unbounder1 <ryadon12@gmail.com>
2023-10-19 22:08:33 -0400

    Added animations react script

--- src/components/hover_animation.jsx
+++ src/components/hover_animation.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
+<<<<<<< Updated upstream
 
 function  HoverComponents(){
     const [hovered, setHovered] = React.useState(false);
+=======
+import PngPlayer from './png_player';
+
+function HoverComponents(props) {
+    const [hovered, setHovered] = useState(false);
+    const styles = {
+        // backgroundImage: `url(${props.name})`,
+        backgroundColor: hovered ? 'white' : '',
+        fontSize: '10vw'
+    }
+>>>>>>> Stashed changes
 
     const handleMouseEnter = () => {
         setHovered(true);
     }
     const handleMouseLeave = () => {
         setHovered(false);
+<<<<<<< Updated upstream
     }
 
     return (
@@ -16,6 +29,27 @@ function  HoverComponents(){
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
         ></div>
+=======
+
+    };
+
+    return (
+        <div
+            style={styles}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <PngPlayer
+                startFrame={parseInt(props.startFrame)}
+                frameRate={parseInt(props.frameRate)}
+                endFrame={parseInt(props.endFrame)}
+                directory={props.directory}
+                name={props.name}
+                direction={hovered ? 'forward' : 'backward'}
+            />
+            {props.message}
+        </div>
+>>>>>>> Stashed changes
     );
 }
 
