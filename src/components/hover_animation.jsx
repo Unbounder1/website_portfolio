8ec1dc8a7a83bd5050f18390e6e28809b1f6cc17
Unbounder1 <ryadon12@gmail.com>
2023-10-22 00:48:51 -0400

    moved png_player into hover_animation
    doesnt work

--- src/components/hover_animation.jsx
+++ src/components/hover_animation.jsx
@@ -1,20 +1,53 @@
 import React, { useState } from 'react';
 import PngPlayer from './png_player';
 
-function HoverComponents(props) {
+function HoverComponents({startFrame, frameRate, endFrame, directory, message}) {
+    startFrame = parseInt(startFrame, 10);
+    endFrame = parseInt(endFrame, 10);
+    frameRate = parseInt(frameRate, 10);
+
     const [hovered, setHovered] = useState(false);
+    const [currentFrame, setCurrentFrame] = useState(startFrame);
+
+    const imageUrl = `${directory}${currentFrame.toString().padStart(2, '0')}-min.png`;
     const styles = {
-        // backgroundImage: `url(${props.name})`,
-        backgroundColor: hovered ? 'white' : '',
+        backgroundImage: `url('${imageUrl}')`,
         fontSize: '10vw'
     }
 
+    const frameDuration = 1000 / frameRate;
+    let timer;
+
     const handleMouseEnter = () => {
         setHovered(true);
+        clearInterval(timer); 
+        timer = setInterval(() => {
+            setCurrentFrame(prevFrame => {
+                const nextFrame = prevFrame + 1;
+                console.log(prevFrame)
+                if (nextFrame < endFrame) {
+                    return nextFrame;
+                }
+                clearInterval(timer);
+                return prevFrame;
+            });
+        }, frameDuration);
     }
+
     const handleMouseLeave = () => {
         setHovered(false);
-
+        clearInterval(timer); 
+        timer = setInterval(() => {
+            setCurrentFrame(prevFrame => {
+                console.log(prevFrame)
+                const prev = prevFrame - 1;
+                if (prev < 0) { 
+                    clearInterval(timer);
+                    return 0;
+                }
+                return prev;
+            });
+        }, frameDuration);
     };
 
     return (
@@ -23,15 +56,7 @@ function HoverComponents(props) {
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
-            <PngPlayer
-                startFrame={parseInt(props.startFrame)}
-                frameRate={parseInt(props.frameRate)}
-                endFrame={parseInt(props.endFrame)}
-                directory={props.directory}
-                name={props.name}
-                direction={hovered ? 'forward' : 'backward'}
-            />
-            {props.message}
+            {message}
         </div>
     );
 }
